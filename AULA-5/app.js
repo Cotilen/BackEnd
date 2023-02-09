7098898cce87c3e05c0eb0812e0ce271a11e09bf
CLEITON DOS SANTOS CRUZ <22282081@CFP127ALUNO.LOCAL>
2023-02-09 09:47:31 -0300

    Aula-4

---
+++ AULA-5/app.js
@@ -0,0 +1,6 @@
+/*******************************************************************
+ * Objetivo: Realizar uma calculadora simples que subtraia, some, multiplique e divida.
+ * Data: 09/02/23
+ * Autor: Cleiton
+ * Versão: 1.0
+ ******************************************************************/
